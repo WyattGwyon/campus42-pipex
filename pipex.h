2d054961ae1503998aed640fb5b6b3190e4b5e94
Clayton Louden <clouden@student.42madrid.com>
2025-08-18 18:45:28 +0200

    function descriptor added as hidden dot file, and header added

---
+++ pipex.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clouden <clouden@student.42madrid.com      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/18 16:28:20 by clouden           #+#    #+#             */
+/*   Updated: 2025/08/18 16:28:25 by clouden          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	arg_parser(int argc, char *argv[]);
+
+# endif
